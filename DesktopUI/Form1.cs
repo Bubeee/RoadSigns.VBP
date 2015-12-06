59272aaef1c6299f30e51ba4031b4005542d67a9
Vladimir <voweer@gmail.com>
2015-12-07 01:53:43 +0300

    rectification

--- DesktopUI/Form1.cs
+++ DesktopUI/Form1.cs
@@ -3,6 +3,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Homography;
 using SurfaceHandling;
 using Filters;
 using System;
@@ -322,147 +323,14 @@ namespace DesktopUI
 
         private void RotateButton_Click(object sender, EventArgs e)
         {
-            var point11 = new Point(74, 66);
-            var point12 = new Point(233, 31);
-            var point13 = new Point(251, 192);
-            var point14 = new Point(39, 173);
-            var point21 = new Point(0, 0);
-            var point22 = new Point(110, 0);
-            var point23 = new Point(110, 90);
-            var point24 = new Point(0, 90);
+            var point11 = new PointF(331, 238);               // входные данные - координаты левой верхней точки 
+            var point12 = new PointF(623, 188);              // правый верх
+            var point13 = new PointF(623, 255);             // правый низ
+            var point14 = new PointF(332, 312);
 
-            var scrPoints = new List<Point> { point11, point12, point13, point14 };
-            var destPoints = new List<Point> { point21, point22, point23, point24 };
-
-            var resultBitmap = new Bitmap(_currentImage.Width, _currentImage.Height);
-
-            for (var i = 0; i < _currentImage.Width; i++)
-            {
-                for (var j = 0; j < _currentImage.Height; j++)
-                {
-                    var point = new Point(i, j);
-                    var newPoint = MultiplyOnMatrix(point, GetHMatrix(scrPoints, destPoints));
-                    resultBitmap.SetPixel(newPoint.X, newPoint.Y, _currentImage.GetPixel(i, j));
-                }
-            }
-
-            resultPictureBox.Image = resultBitmap;
-        }
-
-        private static Point MultiplyOnMatrix(Point srcPoint, int[,] matrix)
-        {
-            var resultVector = new int[3];
-            int[] vector = { srcPoint.X, srcPoint.Y, 1 };
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    resultVector[i] += matrix[i, j] * vector[j];
-                }
-            }
-
-            return new Point(resultVector[0], resultVector[1]);
-        }
-
-        private static int[,] GetHMatrix(List<Point> srcPoints, List<Point> destPoints)
-        {
-            var matrix = GetTransfromMatrix(srcPoints, destPoints);
-            var result = new int[3, 3];
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    result[i, j] = (int)matrix[i][j];
-                }
-            }
-
-            return result;
-        }
-
-        public static double[][] GetTransfromMatrix(List<Point> image, List<Point> carNumber)
-        {
-            var matr = new double[8, 9];
-            for (int i = 0; i < 4; i++)
-            {
-                matr[i, 0] = image[i].X;
-                matr[i, 1] = image[i].Y;
-                matr[i, 2] = 1;
-                matr[i, 3] = 0;
-                matr[i, 4] = 0;
-                matr[i, 5] = 0;
-                matr[i, 6] = -image[i].X * carNumber[i].X;
-                matr[i, 7] = -image[i].Y * carNumber[i].X;
-                matr[i, 8] = carNumber[i].X;
-            }
-
-            for (int i = 4; i < 8; i++)
-            {
-                matr[i, 0] = 0;
-                matr[i, 1] = 0;
-                matr[i, 2] = 0;
-                matr[i, 3] = image[i - 4].X;
-                matr[i, 4] = image[i - 4].Y;
-                matr[i, 5] = 1;
-                matr[i, 6] = image[i - 4].X * carNumber[i - 4].X;
-                matr[i, 7] = image[i - 4].Y * carNumber[i - 4].X;
-                matr[i, 8] = carNumber[i - 4].X;
-            }
-
-            var x = Gaus(matr);
-            var d = new double[3][];
-            for (int i = 0; i < 3; i++)
-            {
-                d[i] = new double[3];
-                for (int j = 0; j < 3; j++)
-                {
-                    d[i][i] = x[i + j];
-                }
-            }
-            d[2][2] = 1;
-            return d;
-        }
-
-        static double[] Gaus(double[,] matr)
-        {
-            double[,] a = matr;
-            int n = 9;
-            //for (int i = 0; i < a.Length / n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        Console.Write(a[i, j].ToString() + "x" + j.ToString() + " ");
-            //    }
-            //    Console.Write("= " + a[i, n - 1].ToString());
-            //    Console.WriteLine("");
-            //}
-            double[] x = new double[a.Length / n];
-            for (int z = 0; z < a.Length / n; z++)
-            {
-                x[z] = a[z, n - 1];
-            }
-            double m;
-            for (int k = 1; k < a.Length / n; k++)
-            {
-                for (int j = k; j < a.Length / n; j++)
-                {
-                    m = a[j, k - 1] / a[k - 1, k - 1];
-                    for (int i = 0; i < n; i++)
-                    {
-                        a[j, i] = a[j, i] - m * a[k - 1, i];
-                    }
-                    x[j] = x[j] - m * x[k - 1];
-                }
-            }
-
-            for (int i = a.Length / n - 1; i >= 0; i--)
-            {
-                for (int j = i + 1; j < a.Length / n; j++) x[i] -= a[i, j] * x[j];
-                x[i] = x[i] / a[i, i];
-            }
-
-            return x;
+            var srcPoints = new PointF[] { point11, point12, point13, point14 };
+          
+            resultPictureBox.Image = Homograph.Rectification(_currentImage, srcPoints);
         }
     }
 }
